4e71839e29f666721f2a14ad5645a7d5e6080fe8
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-07-10 19:54:33 +0200

    updates

---
+++ corefx/src/Lab/RegExPerf/Manual/perf2.cmd
@@ -0,0 +1,3 @@
+corerun ..\..\..\tools\csc.dll /noconfig /noconfig /optimize /r:System.Private.Corelib.dll /r:System.Runtime.dll /r:System.Runtime.Extensions.dll /r:System.Console.dll /r:System.Text.RegularExpressions.dll /r:System.Collections.dll /r:BenchmarkDotNet.dll /r:BenchmarkDotNet.Core.dll  /out:Regexredux.dll Regexredux.cs
+PerfView.exe collect PerfViewData.etl /DotNetAllocSampled
+corerun Regexredux.dll 0 < input5000000.txt
