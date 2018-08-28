e79162c4c79f747d7fe0fd671fa892ccbd7d59ae
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-08-28 20:12:35 +0200

    updates

--- corefx/src/Lab/RegExPerf/Program.cs
+++ corefx/src/Lab/RegExPerf/Program.cs
@@ -1,27 +1,9 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Toolchains.CoreRun;
-using System.IO;
+﻿using BenchmarkDotNet.Running;
 
 namespace RegExPerf
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            // dotnet run -c Release -f netcoreapp2.1 -- -f *Benchmarks* --coreRun "D:\git\corefx\bin\testhost\netcoreapp-Windows_NT-Release-x64\shared\Microsoft.NETCore.App\9.9.9\CoreRun.exe"
-
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-
-            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
-            //   .Run(args, DefaultConfig.Instance.With(
-            //       Job.ShortRun.With(
-            //           new CoreRunToolchain(
-            //               new FileInfo(@"D:\git\corefx\bin\testhost\netcoreapp-Windows_NT-Release-x64\shared\Microsoft.NETCore.App\9.9.9\CoreRun.exe")
-            //               ))));
-
-
-        }
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
