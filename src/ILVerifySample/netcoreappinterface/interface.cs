3980d8b0dfb9448a75ea005eda2833ee42af3d5c
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-09-20 17:05:29 +0200

    updates

--- src/ILVerifySample/netcoreappinterface/interface.cs
+++ src/ILVerifySample/netcoreappinterface/interface.cs
@@ -1,5 +1,7 @@
-﻿public interface I
+﻿public interface Interface
 {
     void M1();
     void M2();
+    void M3(int i);
+    int M4(int i);
 }
--- src/ILVerifySample/netcoreapplib/implementation.cs
+++ src/ILVerifySample/netcoreapplib/implementation.cs
@@ -1,12 +1,22 @@
-﻿public class C : I
+﻿public class Class : Interface
 {
     public void M1()
     {
-
+        
     }
 
     public void M2()
     {
+       
+    }
 
+    public void M3(System.Int32 i)
+    {
+       
+    }
+
+    public System.Int32 M4(System.Int32 i)
+    {
+        return 0;
     }
 }
