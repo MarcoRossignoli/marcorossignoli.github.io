0d745c2c3e6f21a1637c67ee745b6daf32aeb489
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-10-21 11:58:02 +0200

    updates

---
+++ src/BenchmarksPlayground/BenchmarksPlayground/VirtualCall.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarksPlayground
+{
+    [MemoryDiagnoser]
+    [DisassemblyDiagnoser(printPrologAndEpilog: false)]
+    public class VirtualCall
+    {
+        private NoVirtualCallClass _nonVirtual;
+        private VirtualCallClass _virtual;
+
+        public VirtualCall()
+        {
+            _nonVirtual = new NoVirtualCallClass();
+            _virtual = new VirtualCallClass();
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void NoVirtual()
+        {
+            _nonVirtual.Method();
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Virtual()
+        {
+            _virtual.Method();
+        }
+    }
+
+    public sealed class NoVirtualCallClass
+    {
+        private int _i = 0;
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Method()
+        {
+            _i++;
+        }
+    }
+
+    public class VirtualCallClass
+    {
+        private int _i = 0;
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public virtual void Method()
+        {
+            _i++;
+        }
+    }
+}
