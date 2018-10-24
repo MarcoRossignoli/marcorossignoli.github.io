cce6c0948f87a4b1cabf0897d78e5043b17f0cca
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-10-24 18:03:49 +0200

    updates

---
+++ src/ILVerifySample/netcoreappgenericinterface/interface.cs
@@ -0,0 +1,17 @@
+﻿namespace netcoreappgenericinterface
+{
+    public interface IGeneric<TIN, TOUT>
+    {
+        TOUT M1(TIN par, TIN par2);
+    }
+
+
+    public class GenericImplementationInt32 : IGeneric<int, int>
+    {
+        public int M1(int par, int par2)
+        {
+            return 0;
+        }
+    }
+
+}
