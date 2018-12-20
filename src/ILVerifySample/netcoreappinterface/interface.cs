38a66852b6bfd270897fe69fb8e3a30ad7f254bb
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-12-20 20:51:49 +0100

    Ilverify playground (#8)
    ILVerify playground

--- src/ILVerifySample/netcoreappinterface/interface.cs
+++ src/ILVerifySample/netcoreappinterface/interface.cs
@@ -1,4 +1,7 @@
-﻿public interface I
+﻿public interface Interface
 {
-    void M();
+    void M1();
+    void M2();
+    void M3(int i);
+    int M4(int i);
 }
--- src/ILVerifySample/netcoreapplib/implementation.cs
+++ src/ILVerifySample/netcoreapplib/implementation.cs
@@ -1,7 +1,22 @@
-﻿public class C : I
+﻿public class Class : Interface
 {
-    public void M()
+    public void M1()
     {
-        throw new System.NotImplementedException();
+        
+    }
+
+    public void M2()
+    {
+       
+    }
+
+    public void M3(System.Int32 i)
+    {
+       
+    }
+
+    public System.Int32 M4(System.Int32 i)
+    {
+        return 0;
     }
 }
