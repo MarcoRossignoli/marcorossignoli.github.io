d6d31b3b511b1f7cdbd6ca476b8b97acd15b4467
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-12-22 17:33:59 +0100

    updates

--- src/coverlet/Issue1/run.cmd
+++ src/coverlet/Issue1/run.cmd
@@ -1,9 +1,9 @@
 rem dotnet test --configuration Release "test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Api.Tests.xml
-dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.xml /p:Include="[Keys.Application]*"
-dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true   /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.json /p:Include="[Keys.Application]*"
+rem dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.xml /p:Include="[Keys.Application]*"
+rem dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true   /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.json /p:Include="[Keys.Application]*"
 rem test "test\Keys.Data.Tests\Keys.Data.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Data.Tests.xml
 
-reportgenerator -reports:.\test-results\coverage\*.xml -targetdir:.\test-results\report
+rem reportgenerator -reports:.\test-results\coverage\*.xml -targetdir:.\test-results\report
 
 
 rem dotnet test --configuration Release "test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true  /p:CoverletOutput=..\..\test-results\coverage\Keys.Api.Tests.json 
@@ -12,3 +12,7 @@ rem dotnet test --configuration Release "test\Keys.Data.Tests\Keys.Data.Tests.cs
 
 rem dotnet test --configuration Release  /p:CollectCoverage=true /p:CoverletOutput=..\..\test-results\coverage /p:MergeWith='..\..\test-results\coverage\result.json'
 rem reportgenerator -reports:.\test-results\coverage\*.json -targetdir:.\test-results\report
+
+del "D:\git\marcorossignoli.github.io\src\coverlet\Issue1\test-results\coverage\coverage.json" /Y
+dotnet test "D:\git\marcorossignoli.github.io\src\coverlet\Issue1\test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutput=..\..\test-results\coverage\ /p:MergeWith=..\..\test-results\coverage\coverage.json
+dotnet test "D:\git\marcorossignoli.github.io\src\coverlet\Issue1\test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutput=..\..\test-results\coverage\ /p:MergeWith=..\..\test-results\coverage\coverage.json
