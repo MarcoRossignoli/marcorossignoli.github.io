1b33e6ecf8480e4fbe47166e4c03a3f5163b78a0
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-12-22 16:11:01 +0100

    updates

--- src/coverlet/Issue1/run.cmd
+++ src/coverlet/Issue1/run.cmd
@@ -1,5 +1,14 @@
-dotnet test "test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Api.Tests.xml
-#dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.xml
-#dotnet test "test\Keys.Data.Tests\Keys.Data.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Data.Tests.xml
+rem dotnet test --configuration Release "test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Api.Tests.xml
+dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.xml /p:Include="[Keys.Application]*"
+dotnet test "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true   /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.json /p:Include="[Keys.Application]*"
+rem test "test\Keys.Data.Tests\Keys.Data.Tests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover  /p:CoverletOutput=..\..\test-results\coverage\Keys.Data.Tests.xml
 
 reportgenerator -reports:.\test-results\coverage\*.xml -targetdir:.\test-results\report
+
+
+rem dotnet test --configuration Release "test\Keys.Api.Tests\Keys.Api.Tests.csproj" /p:CollectCoverage=true  /p:CoverletOutput=..\..\test-results\coverage\Keys.Api.Tests.json 
+rem dotnet test --configuration Release "test\Keys.Application.Tests\Keys.Application.Tests.csproj" /p:CollectCoverage=true  /p:CoverletOutput=..\..\test-results\coverage\Keys.Application.Tests.json 
+rem dotnet test --configuration Release "test\Keys.Data.Tests\Keys.Data.Tests.csproj" /p:CollectCoverage=true  /p:CoverletOutput=..\..\test-results\coverage\Keys.Data.Tests.json 
+
+rem dotnet test --configuration Release  /p:CollectCoverage=true /p:CoverletOutput=..\..\test-results\coverage /p:MergeWith='..\..\test-results\coverage\result.json'
+rem reportgenerator -reports:.\test-results\coverage\*.json -targetdir:.\test-results\report
--- src/coverlet/Issue1/test/Keys.Application.Tests/KeyProviderTests.cs
+++ src/coverlet/Issue1/test/Keys.Application.Tests/KeyProviderTests.cs
@@ -6,7 +6,6 @@ using Keys.Data.Enums;
 using Keys.Data.Repositories.Contracts;
 using Moq;
 using Xunit;
-using Key = Keys.Data.Entities.Key;
 
 namespace Keys.Application.Tests
 {
@@ -29,14 +28,7 @@ namespace Keys.Application.Tests
         [InlineData(KeyType.Enum2)]
         public async Task GetByUuidAndTypeAsync01(KeyType keyType)
         {
-            _keyRepositoryMock.Setup(kr =>
-                    kr.FindOrCreateAsync(_testGuid, keyType, It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new Key());
-
             await _keyProvider.GetByUuidAndTypeAsync(_testGuid, keyType, new CancellationToken());
-
-            _keyRepositoryMock.Verify(
-                kr => kr.FindOrCreateAsync(_testGuid, keyType, It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
