1b6135563aa3a4ddc1f2ae8ac1e0d6015d800555
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-01-18 09:04:28 +0100

    update git commands

---
+++ src/git-commands/git-cleanup.cmd
@@ -0,0 +1,7 @@
+taskkill /IM dotnet.exe /F
+taskkill /IM vscscompiler.exe /F
+taskkill /IM msbuild.exe /F
+git clean -fdx
+
+@rem To install on Win
+@rem git config --global alias.cleanup "!C:/git/marcorossignoli.github.io/src/git-commands/git-cleanup.cmd"
---
+++ src/git-commands/installcommands.cmd
@@ -0,0 +1,4 @@
+git config --global alias.cleanup "!F:/git/marcorossignoli.github.io/src/git-commands/git-cleanup.cmd"
+git config --global alias.commitall "!F:/git/marcorossignoli.github.io/src/git-commands/git-commitall.cmd"
+git config --global alias.squashall "!F:/git/marcorossignoli.github.io/src/git-commands/git-squashall.cmd"
+git config --global alias.syncsource "!F:/git/marcorossignoli.github.io/src/git-commands/git-syncsource.cmd"
