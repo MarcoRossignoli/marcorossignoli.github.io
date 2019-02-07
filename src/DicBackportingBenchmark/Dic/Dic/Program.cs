350c67f507d5c8889cbb09758079d734a4932f35
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-02-07 17:51:07 +0100

    updates

--- src/DicBackportingBenchmark/Dic/Dic/Program.cs
+++ src/DicBackportingBenchmark/Dic/Dic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -9,9 +10,15 @@ namespace ConsoleApp1
             System.Collections.Generic2.Dictionary<string, string> test = new System.Collections.Generic2.Dictionary<string, string>();
             for (int i = 0; i < 100; i++)
             {
+                Console.WriteLine(i);
                 var key = Guid.NewGuid().ToString("D");
                 test.Add(key, "V_" + i);
                 var r = test[key];
+            }
+
+            foreach (var item in test.ToArray())
+            {
+                test.Remove(item.Key);
             }
         }
     }
