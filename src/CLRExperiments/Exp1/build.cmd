81c7871dc64aafedf734795d541a7716f03b54d9
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-04-25 21:37:17 +0200

    updates

--- src/CLRExperiments/Exp1/build.cmd
+++ src/CLRExperiments/Exp1/build.cmd
@@ -1,6 +1,18 @@
 SET COMPlus_JitDisasm=Check
+SET COMPlus_JitDump=
 SET COMPlus_TieredCompilation_Test_OptimizeTier0=1
 SET COMPlus_JitDiffableDasm=1
 dotnet publish -c release
 copy /y %~d0\git\coreclr\bin\Product\Windows_NT.x64.Debug\clrjit.dll %~dp0bin\Release\netcoreapp3.0\win-x64\publish\
-%~dp0bin\Release\netcoreapp3.0\win-x64\publish\Exp1.exe
+%~dp0bin\Release\netcoreapp3.0\win-x64\publish\Exp1.exe > %~dp0bin\Release\netcoreapp3.0\win-x64\publish\jitdasm.txt
+start notepad.exe %~dp0bin\Release\netcoreapp3.0\win-x64\publish\jitdasm.txt
+
+
+SET COMPlus_JitDisasm=
+SET COMPlus_JitDump=Check
+SET COMPlus_TieredCompilation_Test_OptimizeTier0=1
+SET COMPlus_JitDiffableDasm=1
+dotnet publish -c release
+copy /y %~d0\git\coreclr\bin\Product\Windows_NT.x64.Debug\clrjit.dll %~dp0bin\Release\netcoreapp3.0\win-x64\publish\
+%~dp0bin\Release\netcoreapp3.0\win-x64\publish\Exp1.exe > %~dp0bin\Release\netcoreapp3.0\win-x64\publish\jitdump.txt
+start notepad.exe %~dp0bin\Release\netcoreapp3.0\win-x64\publish\jitdump.txt
