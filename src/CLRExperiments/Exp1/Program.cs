141d7c7478170269a7bc4d8dd447099a616de787
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-05-28 16:10:16 +0200

    updates

--- src/CLRExperiments/Exp1/Program.cs
+++ src/CLRExperiments/Exp1/Program.cs
@@ -4,6 +4,7 @@ namespace Clr
 {
     // https://github.com/dotnet/coreclr/blob/master/Documentation/building/debugging-instructions.md
     // https://github.com/dotnet/coreclr/blob/master/Documentation/botr/ryujit-overview.md
+    // https://github.com/dotnet/coreclr/blob/04fed62162092da2a824e425aa65b8fcfc70ce14/Documentation/botr/ryujit-overview.md#phases-of-ryujit
     class Program
     {
         static void Main(string[] args)
