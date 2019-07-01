7f041141be33ffcdb436091fc2e51db14c0f67ee
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-07-01 17:54:42 +0200

    updates

---
+++ src/Interview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
