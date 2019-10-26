3c34a74a5a05115fc38eceb858dcea38315306c3
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-10-26 17:04:56 +0200

    updates

--- src/CLRExperiments/Exp1/build.cmd
+++ src/CLRExperiments/Exp1/build.cmd
@@ -13,7 +13,6 @@ SET COMPlus_JitDisasm=
 SET COMPlus_JitDump=Check CheckOptimized
 SET COMPlus_TieredCompilation_Test_OptimizeTier0=1
 SET COMPlus_JitDiffableDasm=1
-echo SET JITMinOpts=1
 dotnet publish -c release
 robocopy /e %~d0\git\coreclr\bin\Product\Windows_NT.x64.Release %~dp0bin\Release\netcoreapp3.0\win-x64\publish\ > nul
 copy /y %~d0\git\coreclr\bin\Product\Windows_NT.x64.Debug\clrjit.dll %~dp0bin\Release\netcoreapp3.0\win-x64\publish\
