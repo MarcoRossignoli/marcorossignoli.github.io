b8fee2c768a2123b06dbdb985ea7abdc29e0c833
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-11-30 11:00:31 +0100

    Test (#31)

--- src/coverlet/TestLocalPackages/dotnettool.cmd
+++ src/coverlet/TestLocalPackages/dotnettool.cmd
@@ -1,4 +1,4 @@
-dotnet tool install coverlet.console --global --version 1.6.31-geaaf0afc31
+dotnet tool install coverlet.console --global --version 1.6.31-g2a731d58f2
 
 dotnet build
 coverlet XUnitTestProject1\bin\Debug\netcoreapp2.1\XUnitTestProject1.dll --target "dotnet" --targetargs "test XUnitTestProject1\XUnitTestProject1.csproj --no-build"
