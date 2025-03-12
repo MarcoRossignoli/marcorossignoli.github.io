61eccea964e8501a26b96969bd712a2589722489
Marco Rossignoli <mrossignol@microsoft.com>
2025-03-12 15:02:20 +0100

    Add sample

---
+++ src/Calc/src/Calculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }   
+    }
+}
---
+++ src/Calc/tests/Calculator.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(4, calculator.Add(2, 2));
+        }
+    }
+}
